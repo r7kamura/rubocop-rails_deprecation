8a029381cdfb44e8f41f401a56be9671a8bb08a0
Ryo Nakamura <r7kamura@gmail.com>
2022-02-23 05:03:08 +0900

    Run rubocop-extension-generator rubocop-rails_deprecation

---
+++ lib/rubocop/rails_deprecation.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rails_deprecation/version"
+
+module Rubocop
+  module RailsDeprecation
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
