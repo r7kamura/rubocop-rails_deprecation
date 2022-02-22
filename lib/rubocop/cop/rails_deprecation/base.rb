b408a0afaeafcc4c9206d3c0c06296f26e12d98f
Ryo Nakamura <r7kamura@gmail.com>
2022-02-23 06:20:21 +0900

    Add RailsDeprecation/ToFormattedS cop

---
+++ lib/rubocop/cop/rails_deprecation/base.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RailsDeprecation
+      class Base < ::RuboCop::Cop::Base
+        DEFAULT_MINIMUM_TARGET_RAILS_VERSION = 5.0
+
+        class << self
+          attr_writer :minimum_target_rails_version
+
+          # @return [Float]
+          def minimum_target_rails_version
+            @minimum_target_rails_version ||= DEFAULT_MINIMUM_TARGET_RAILS_VERSION
+          end
+
+          # @note Called from `RuboCop::Cop::Team#support_target_rails_version?`.
+          # @param [Float] version
+          # @return [Boolean]
+          def support_target_rails_version?(version)
+            minimum_target_rails_version <= version
+          end
+        end
+      end
+    end
+  end
+end
