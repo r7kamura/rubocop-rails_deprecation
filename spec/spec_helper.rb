3d1f2f9ee3bbf8b8e8735c5935ae04088d899e01
Ryo Nakamura <r7kamura@gmail.com>
2022-02-23 06:44:56 +0900

    Set up RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,4 @@ RSpec.configure do |config|
   config.raise_errors_for_deprecations!
   config.raise_on_warning = true
   config.fail_if_no_examples = true
-
-  config.order = :random
-  Kernel.srand config.seed
 end
