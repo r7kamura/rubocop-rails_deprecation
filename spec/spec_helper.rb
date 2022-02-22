e8a9d493c77de03beff8615f42c78df1f57ba5a3
Ryo Nakamura <r7kamura@gmail.com>
2022-02-23 06:55:18 +0900

    Update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,8 @@ RSpec.configure do |config|
   config.include RuboCop::RSpec::ExpectOffense
 
   config.disable_monkey_patching!
+
   config.raise_errors_for_deprecations!
+
   config.raise_on_warning = true
-  config.fail_if_no_examples = true
 end
